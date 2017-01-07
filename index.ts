a400a298f3dfd79bc083725b3eaf7fa7699f4309
Kumar Harsh <khs@playlyfe.com>
2017-01-08 04:38:43 +0530

    first version, setup travis.yml
    TODO: check semantic release

---
+++ index.ts
@@ -0,0 +1,13 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import { workspace, Disposable, ExtensionContext } from 'vscode';
+
+const extName = 'ci-test';
+
+export function activate(context: ExtensionContext) {
+  console.log("STARTING CLIENT");
+}
---
+++ scripts/getLastRelease.js
@@ -0,0 +1,3 @@
+module.exports = function(pluginConfig, {pkg, npm, plugins, options}, cb) {
+  cb(null, {version: '0.1.0'});
+}
