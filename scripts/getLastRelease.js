d1fbd4bc68cde66d33a625611b5e85fe2c6fdac3
Kumar Harsh <khs@playlyfe.com>
2017-01-08 06:23:01 +0530

    fix: update last release version

--- scripts/getLastRelease.js
+++ scripts/getLastRelease.js
@@ -1,6 +1,6 @@
 module.exports = function(pluginConfig, {pkg, npm, plugins, options}, cb) {
   cb(null, {
-    version: '1.0.0',
+    version: '1.0.3',
     // gitHead: data.versions[version].gitHead,
     get tag () {
       npmlog.warn('deprecated', 'tag will be removed with the next major release')
